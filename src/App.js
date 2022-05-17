acf87f4d24e4135e85a1ae739ff831387f1bdf72
rice <pentappingminh@gmail.com>
2022-05-17 20:52:43 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  //1
+  const [data, setData] = useState([]); //2
+  const [isFetchData, setIsFetchData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  //useState, useEffect, Khái niệm cơ bản về bất đồng bộ ( async await), axios, loading using state, list key, condition rendering
+  const getData = async () => {
+    setIsLoading(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (response) {
+        // Nếu lấy thành công thì làm
+        console.log(response);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // Nếu lỗi thì làm
+        console.log(error);
+      })
+      .then(function () {
+        // Luôn luôn làm
+        setIsLoading(false);
+      });
+  };
+  const handleClick = () => {
+    //thêmm mới
+    setIsFetchData(true);
+  };
+  useEffect(() => {
+    getData();
+  }, []); //array ddi
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((val, idx) => (
+                <tr key={idx}>
+                  <td>{val.id}</td>
+                  <td>{val.name}</td>
+                  <td>{val.username}</td>
+                  <td>{val.email}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
 
